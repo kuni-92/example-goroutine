a000bc41e6bc25d41a3b3cbd819cd2049443a113
kuni <hr.9292.1283@gmail.com>
2021-12-08 22:05:07 +0900

    wait sub routine

--- main.go
+++ main.go
@@ -8,14 +8,17 @@ import (
 func main() {
 	fmt.Println("Start!!!")
 
+	quit := make(chan bool)
 	go func() {
 		for index := 0; index < 10; index++ {
 			fmt.Printf("I'm sub routine...(count %d)\n", index)
 			time.Sleep(2 * time.Second)
 		}
+		quit <- true
 	}()
 	for index := 0; index < 10; index++ {
 		fmt.Printf("I'm main routine...(count %d)\n", index)
 		time.Sleep(1 * time.Second)
 	}
+	<-quit
 }
