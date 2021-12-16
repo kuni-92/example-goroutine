e11e417d8336c85d24c86b826a323a5c45816e1c
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2021-12-16 22:22:21 +0900

    Run the two goroutines in sequence.

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
@@ -13,10 +12,9 @@ func main() {
 	go func() {
 		name := "sub"
 		for index := 0; index < 10; index++ {
-			<-ch
 			fmt.Printf("%s >", name)
 			fmt.Printf("I'm %s routine...(count %d)\n", name, index)
-			time.Sleep(time.Second)
+		ch <- true
 		}
 	}()
 
@@ -24,7 +22,6 @@ func main() {
 	for index := 0; index < 10; index++ {
 		fmt.Printf("%s >", name)
 		fmt.Printf("I'm %s routine...(count %d)\n", name, index)
-		ch <- true
-		time.Sleep(time.Second)
+	<-ch
 	}
 }
